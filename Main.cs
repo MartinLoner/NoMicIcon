e67588b7607dc7b7f8a4211ddfeb010afdbb4ae0
Ogata <75475473+ogatasaan@users.noreply.github.com>
2020-12-19 03:04:17 +1000

    Add files via upload

---
+++ Main.cs
@@ -0,0 +1,23 @@
+﻿using MelonLoader;
+using System;
+using System.Reflection;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using System.Collections;
+using UnhollowerRuntimeLib;
+using System.Linq;
+using Harmony;
+using MelonLoader.Tomlyn;
+
+namespace NoMicIcon
+{
+    public class MyMod : MelonMod
+    {
+        public override void VRChat_OnUiManagerInit()
+        {
+            GameObject.DestroyImmediate(GameObject.Find("UserInterface/UnscaledUI/HudContent/Hud/VoiceDotParent"));
+        }
+    }
+}
